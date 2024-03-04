2f87879371cd0e17f6e90da5ccce50a05a2e86cf
cfhaak <c.f.haak@posteo.de>
2024-03-04 16:39:38 +0100

    now getting jsons used for indexing from data repo

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,19 +1,17 @@
-# bin/bash
-
+#! /bin/bash
+json_dir="./json"
+editions_dir="./data/editions/"
 echo "fetching transkriptions"
 # prepare folders
-rm -rf data/editions && mkdir data/editions
-# #rm -rf data/indices && mkdir data/indices
-# #rm -rf data/meta && mkdir data/meta
+rm -rf $editions_dir && mkdir $editions_dir
 # get the data
 (cd ../flugblaetter_data/ && \
     ./shellscripts/extract_infos.sh
-) && cp ../todesurteile-daten/303_annot_tei/*.xml ./data/editions/
+) && cp ../todesurteile-daten/303_annot_tei/*.xml $editions_dir
+if [ -d $json_dir ]
+then
+    rm -r $json_dir
+fi
+mkdir $json_dir
+cp ../flugblaetter_data/out/*.json $json_dir
 
-# ## distribute the data
-# #mv ./dse-static-data-main/data/editions/ ./data
-# #mv ./dse-static-data-main/data/indices/ ./data
-# #mv ./dse-static-data-main/data/meta/ ./data
-# ## clean up
-# #rm main.zip
-# #rm -rf ./dse-static-data-main
