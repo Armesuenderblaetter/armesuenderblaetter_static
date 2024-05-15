6e894cf4fd72a17b34ef84f6ce10bb4a6956646b
cfhaak <c.f.haak@posteo.de>
2024-05-15 13:25:10 +0200

    changed fetch to also get index, see #4

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,17 +1,11 @@
 #! /bin/bash
-json_dir="./json"
 editions_dir="./data/editions/"
+indices_dir="./data/indices/"
 echo "fetching transkriptions"
-# prepare folders
-rm -rf $editions_dir && mkdir $editions_dir
 # get the data
 (cd ../flugblaetter_data/ && \
     ./shellscripts/extract_infos.sh
-) && cp ../flugblaetter_data/todesurteile_master/303_annot_tei/*.xml $editions_dir
-if [ -d $json_dir ]
-then
-    rm -r $json_dir
-fi
-mkdir $json_dir
-cp ../flugblaetter_data/out/*.json $json_dir
+) && cp ../flugblaetter_data/todesurteile_master/303_annot_tei/*.xml $editions_dir &&
+cp -r ../flugblaetter_data/out/xml/* $indices_dir
+cp -r ../flugblaetter_data/out/json .
 
