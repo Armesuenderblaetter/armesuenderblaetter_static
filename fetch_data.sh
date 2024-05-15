47b5cf518e681802d58784d2409b4f9b1a1703ff
cfhaak <c.f.haak@posteo.de>
2024-05-15 15:13:34 +0200

    new filestruckture

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
-editions_dir="./data/editions/"
-indices_dir="./data/indices/"
+data_dir="./data/"
 echo "fetching transkriptions"
 # get the data
 (cd ../flugblaetter_data/ && \
     ./shellscripts/extract_infos.sh
-) && cp ../flugblaetter_data/todesurteile_master/303_annot_tei/*.xml $editions_dir &&
-cp -r ../flugblaetter_data/out/xml/* $indices_dir
+) && cp -r ../flugblaetter_data/out/xml/editions $data_dir &&
+cp -r ../flugblaetter_data/out/xml/index $data_dir
 cp -r ../flugblaetter_data/out/json .
 
