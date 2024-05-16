66d02929e025dcf2d22c5105c8343090ce8cc17a
cfhaak <c.f.haak@posteo.de>
2024-05-16 16:02:39 +0200

    using default stuff

--- fetch_data.sh
+++ fetch_data.sh
@@ -5,6 +5,6 @@ echo "fetching transkriptions"
 (cd ../flugblaetter_data/ && \
     ./shellscripts/extract_infos.sh
 ) && cp -r ../flugblaetter_data/out/xml/editions $data_dir &&
-cp -r ../flugblaetter_data/out/xml/index $data_dir
+cp -r ../flugblaetter_data/out/xml/indices $data_dir
 cp -r ../flugblaetter_data/out/json .
 
