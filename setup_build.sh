aa30ea1f0f5b05f87664b7d8f2dea4e28faa680b
cfhaak <c.f.haak@posteo.de>
2024-06-03 11:34:25 +0200

    added meta dir

--- setup_build.sh
+++ setup_build.sh
@@ -4,5 +4,6 @@ mkdir $data_dir
 cp -r data_repo/xml/editions $data_dir
 cp -r data_repo/xml/indices $data_dir
 cp -r data_repo/json .
+cp -r meta $data_dir
 python ./pyscripts/setup_typesense.py
 python ./pyscripts/setup_person_typesense.py
