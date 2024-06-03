b8745f2f43f6685cc6fb778adac258931a5fe205
cfhaak <c.f.haak@posteo.de>
2024-06-03 12:27:26 +0200

    removed meta-dir bs

--- setup_build.sh
+++ setup_build.sh
@@ -4,6 +4,5 @@ mkdir $data_dir
 cp -r data_repo/xml/editions $data_dir
 cp -r data_repo/xml/indices $data_dir
 cp -r data_repo/json .
-cp -r meta $data_dir
 python ./pyscripts/setup_typesense.py
 python ./pyscripts/setup_person_typesense.py
