51198a10b0526e4bc4419edac981a37978696c34
cfhaak <c.f.haak@posteo.de>
2024-06-03 11:29:31 +0200

    fixed paths & shebang

--- setup_build.sh
+++ setup_build.sh
@@ -1,8 +1,8 @@
-#! /bin/bash
+#!/bin/bash
 data_dir="./data/"
 mkdir $data_dir
-cp data_repo/out/xml/editions $data_dir
-cp -r data_repo/out/xml/indices $data_dir
-cp -r data_repo/out/json .
+cp -r data_repo/xml/editions $data_dir
+cp -r data_repo/xml/indices $data_dir
+cp -r data_repo/json .
 python ./pyscripts/setup_typesense.py
 python ./pyscripts/setup_person_typesense.py
