fb2c16d572cf2ac37b849c12452aab9db3348c2a
cfhaak <c.f.haak@posteo.de>
2024-06-19 13:19:59 +0200

    made wf more verbose

--- fetch_data.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-data_dir="./data/"
-echo "fetching transkriptions"
-# get the data
-(cd ../flugblaetter_data/ && \
-    ./shellscripts/extract_infos.sh
-) && cp -r ../flugblaetter_data/out/xml/editions $data_dir &&
-cp -r ../flugblaetter_data/out/xml/indices $data_dir
-cp -r ../flugblaetter_data/out/json .
