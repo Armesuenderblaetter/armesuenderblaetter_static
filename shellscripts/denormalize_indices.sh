29fbd925f132adad3923aa409f3b1627c96c9049
cfhaak <c.f.haak@posteo.de>
2024-08-22 13:37:13 +0200

    tried it with denormalize-indices but this aint working since the events are only linked in the person-index, so this would just copy the index into the back of the other index; see #73

--- shellscripts/denormalize_indices.sh
+++ shellscripts/denormalize_indices.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 denormalize-indices -f "./data/editions/*.xml" -i "./data/indices/listperson.xml" -x ".//tei:title[1]/text()"
+# denormalize-indices -f "./data/indices/listperson.xml" -i "./data/indices/events.xml" -x ".//tei:title[1]/text()"
