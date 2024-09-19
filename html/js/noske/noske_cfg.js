602f7c403275919be57154634a495f44d05b180a
cfhaak <c.f.haak@posteo.de>
2024-09-19 13:38:32 +0200

    css back, see #78

--- html/js/noske/noske_cfg.js
+++ html/js/noske/noske_cfg.js
@@ -9,51 +9,51 @@ search.search({
     corpname: "flugblaetter",
     attrs: "word,lemma,pos,vocab,id",
     structs: "doc,head,p,lg,l,placeName,quote,bibl,persName,date,cit,g",
-    refs: "doc.id,doc.date, doc.name",
+    // refs: "doc.id,doc.date, doc.name",
+    refs: "doc.id,doc.title",
   },
   hits: {
     id: "custom-noske-hits",
-    // css: {
-    //   table: "noske-hits-table",
-    //   div: "noske-hits-div",
-    //   table: "noske-hits-table",
-    //   thead: "noske-hits-head",
-    //   trHead: "noske-hits-trHead",
-    //   th: "noske-hits-th",
-    //   tbody: "noske-hits-tbody",
-    //   trBody: "noske-hits-trBody",
-    //   td: "noske-hits-td",
-    //   kwic: "noske-hits-kwic",
-    //   left: "noske-hits-left",
-    //   right: "noske-hits-right",
-    // },
+    css: {
+      table: "noske-hits-table",
+      div: "noske-hits-div",
+      thead: "noske-hits-head",
+      trHead: "noske-hits-trHead",
+      th: "noske-hits-th",
+      tbody: "noske-hits-tbody",
+      trBody: "noske-hits-trBody",
+      td: "noske-hits-td",
+      kwic: "noske-hits-kwic",
+      left: "noske-hits-left",
+      right: "noske-hits-right",
+    },
   },
   pagination: {
     id: "custom-noske-pagination",
-    // css: {
-    //   div: "noske-pagination-div",
-    //   select: "noske-pagination-select",
-    // },
+    css: {
+      div: "noske-pagination-div",
+      select: "noske-pagination-select",
+    },
   },
   searchInput: {
     id: "custom-noske-input",
     placeholder: "suchen",
-    // css: {
-    //   div: "noske-search-div",
-    //   select: "noske-search-select",
-    //   input: "noske-search-input",
-    // },
+    css: {
+      div: "noske-search-div",
+      select: "noske-search-select",
+      input: "noske-search-input",
+    },
   },
   stats: {
     id: "custom-noske-stats",
-    // css: {
-    //   div: "stats-div",
-    //   label: "stats-label",
-    // },
+    css: {
+      div: "stats-div",
+      label: "stats-label",
+    },
   },
-  // config: {
-  //   customUrl: "./",
-  //   // urlparam: "wlmaxitems=1",
-  // },
+  config: {
+    customUrl: "./*.html",
+    urlparam: "wlmaxitems=1",
+  },
 });
 search.minQueryLength=1;
