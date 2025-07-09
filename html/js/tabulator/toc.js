862f05591cd993001468f35513125496bc79845d
fsanzl <>
2025-07-09 13:39:22 +0200

    tables

--- html/js/tabulator/toc.js
+++ html/js/tabulator/toc.js
@@ -5,6 +5,6 @@ var config = {
     dataLoader: true,
     columns:[
         {title:"Titel", field:"titel", headerFilter:"input", formatter:"html", download:"false", editor:"input", headerSort:"true"}, //add input editor to the name column
-        {title:"Datum", field:"datum", data:"eventDate", headerFilter:true, width: 300, hozAlign: "center", headerSort:"true"}, //add header filters to the age column
+        {title:"Datum", field:"datum", headerFilter:true, width: 300, hozAlign: "center", headerSort:"true"}, //add header filters to the age column
     ],
 };
