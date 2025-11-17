c2f3739423d15031d8f97c0e710bf76574fc9b9c
fsanzl <>
2025-11-17 10:32:17 +0100

    z`

--- html/js/tabulator/toc.js
+++ html/js/tabulator/toc.js
@@ -2,6 +2,21 @@ const iiif_server_base_path =
   "https://iiif.acdh.oeaw.ac.at/iiif/images/todesurteile/";
 const iiif_attribs = "/full/260,/0/default.jpg";
 
+function htmlStringSorter(a, b) {
+  const toText = (value) => {
+    if (value == null) {
+      return "";
+    }
+    const div = document.createElement("div");
+    div.innerHTML = value;
+    return div.textContent ? div.textContent.trim() : "";
+  };
+
+  const textA = toText(a);
+  const textB = toText(b);
+
+  return textA.localeCompare(textB, "de", { sensitivity: "base" });
+}
 
 var config = {
     //height: 800,
@@ -11,7 +26,7 @@ var config = {
     dataLoader: true,
     columns:[
         {title:"", field:"", formatter:"html", download:"false", headerSort:"false"},
-        {title:"Titel", field:"titel", widthGrow: 3, headerFilter:"input", formatter:"html", download:"false", headerSort:"true"}, //add input editor to the name column
+        {title:"Titel", field:"titel", widthGrow: 3, headerFilter:"input", formatter:"html", download:"false", headerSort:"true", sorter: htmlStringSorter}, //add input editor to the name column
         {title:"Datum", field:"datum",  widthGrow: 1, headerFilter:true, hozAlign: "center", headerSort:"true"}, //add header filters to the age column
         {title:"Ort", field:"ort",  widthGrow: 1, headerFilter:true, hozAlign: "center", headerSort:"true"}, //add header filters to the age column
     ],
